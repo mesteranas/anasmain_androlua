53515d3294c2fe311698a98ccbbbe66d732e3e61
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-07-24 17:00:15 +0300

    24/07/2024

---
+++ anasmain/main.lua
@@ -0,0 +1,10 @@
+require "import"
+import "android.widget.*"
+import "android.view.*"
+name=""
+layout={
+    LinearLayout;
+}
+dlg=activity
+dlg.setTitle(name)
+dlg.setContentView(loadlayout(layout))
